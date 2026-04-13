7e2c2da977fd23458800eed950a07e24d74507ec
Julian <julianmoran1@gmail.com>
2026-04-13 16:54:56 -0300

    stream

--- src/pages/DetailSummarizeButton.jsx
+++ src/pages/DetailSummarizeButton.jsx
@@ -5,19 +5,29 @@ export function DetailSummarizeButton({ id }) {
 
   const [summary, setSummary] = useState(null)
   const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(false)
+  const [ , setError] = useState(false)
 
   async function generateSummary() {
     setLoading(true)
     setError(null)
+    setSummary('')
 
     try {
       const response = await fetch(`https://devjobs-back.vercel.app/ia/summary/${id}`)
       if (!response.ok) {
         throw new Error('Error fetching summary')
       }
-      const data = await response.json()
-      setSummary(data.summary)
+      // const data = await response.json()
+      const reader = response.body.getReader()
+      const decoder = new TextDecoder()
+
+      while (true) {
+        const { done, value } = await reader.read()
+        if (done) break
+
+        const chunkText = decoder.decode(value, { stream: true})
+        setSummary(prev => prev + chunkText)
+      }
     } catch {
       console.log('Error al generar resumen')
     }
