9d27b023a2532fc7af96676a6ee6f17399f2a5ce
Julian <julianmoran1@gmail.com>
2026-04-13 17:19:51 -0300

    refactor logic

--- src/pages/DetailSummarizeButton.jsx
+++ src/pages/DetailSummarizeButton.jsx
@@ -1,40 +1,9 @@
-import { useState } from 'react'
+import { useAISummary } from '../hooks/useAISummary'
 import styles from './Detail.module.css'
 
 export function DetailSummarizeButton({ id }) {
 
-  const [summary, setSummary] = useState(null)
-  const [loading, setLoading] = useState(false)
-  const [ , setError] = useState(false)
-
-  async function generateSummary() {
-    setLoading(true)
-    setError(null)
-    setSummary('')
-
-    try {
-      const response = await fetch(`https://devjobs-back.vercel.app/ia/summary/${id}`)
-      if (!response.ok) {
-        throw new Error('Error fetching summary')
-      }
-      // const data = await response.json()
-      const reader = response.body.getReader()
-      const decoder = new TextDecoder()
-
-      while (true) {
-        const { done, value } = await reader.read()
-        if (done) break
-
-        const chunkText = decoder.decode(value, { stream: true})
-        setSummary(prev => prev + chunkText)
-      }
-    } catch {
-      console.log('Error al generar resumen')
-    }
-    finally {
-      setLoading(false)
-    }
-  }
+  const { loading, summary, generateSummary }  = useAISummary({ id })
 
   return (
     <>
